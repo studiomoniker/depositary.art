79b3b6c416de7df916b8729f57c58d45968c351e
Thomas Boland <thomas@thomasjonas.nl>
2023-04-06 11:14:45 +0200

    ability to select default archive

--- site/src/routes/+page.ts
+++ site/src/routes/+page.ts
@@ -1,5 +1,13 @@
-import { redirect } from '@sveltejs/kit'
+import { get } from 'svelte/store'
+import { load_Homepage } from '$houdini'
+import { redirect, error } from '@sveltejs/kit'
 
-export function load() {
-	throw redirect(302, '/test-archive')
+export async function load(event) {
+	const { Homepage } = await load_Homepage({ event })
+
+	const { data } = get(Homepage)
+	if (!data) error('Could not load homepage data')
+
+	throw redirect(302, `/${data?.homepage?.archive?.slug}`)
+	return { Homepage }
 }
