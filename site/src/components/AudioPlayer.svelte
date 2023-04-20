741a3b30acdfa077e2227a9b73613c9d37f9ef2a
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-04-20 13:42:44 +0200

    here we go again…

--- site/src/components/AudioPlayer.svelte
+++ site/src/components/AudioPlayer.svelte
@@ -5,15 +5,20 @@
 	const url = `${import.meta.env.VITE_CMS}/assets/${audioFileId}?access_token=${
 		import.meta.env.VITE_AUTH_TOKEN
 	}`
+
+	let audioElement: HTMLAudioElement | undefined = undefined
+	$: if ($interactedWithPage && !$preferences.muted) {
+		audioElement?.play()
+	} else {
+		audioElement?.pause()
+	}
 </script>
 
-{#if $interactedWithPage && !$preferences.muted && url}
-	<audio
-		src={url}
-		style="opacity: 0.01; position: absolute; width: 0; height: 0; overflow: hidden;"
-		loop
-		playsinline
-		preload="auto"
-		autoplay
-	/>
-{/if}
+<audio
+	src={url}
+	bind:this={audioElement}
+	style="opacity: 0.01; position: absolute; width: 0; height: 0; overflow: hidden;"
+	loop
+	playsinline
+	preload="auto"
+/>
